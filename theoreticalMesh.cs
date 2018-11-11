a6f8b7bc1c3d5f9c98e603ad7f2912afff4efc1e
q_andrew <andrewmanq@gmail.com>
2018-11-11 16:13:21 -0500

    Uploaded original scripts
    theoreticalMesh.cs is the implementation, theoreticalMeshTester.cs tests each method.

---
+++ theoreticalMesh.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class theoreticalMesh : MonoBehaviour {
+
+    private List<Vector3> verts;
+    private List<int> tris;
+
+    public theoreticalMesh()
+    {
+        verts = new List<Vector3>();
+        tris = new List<int>();
+    }
+
+    public theoreticalMesh(Mesh aMesh)
+    {
+        adopt(aMesh);
+    }
+
+    public void addNgon(Vector3 center, List<Vector3> points)
+    {
+        int theSize = points.Count;
+        for (int i = 0; i < theSize; i++)
+        {
+            addTriangle(center, points[i], points[(i + 1) % theSize]);
+        }
+    }
+
+    public void addCube(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4,  //top four
+        Vector3 p5, Vector3 p6, Vector3 p7, Vector3 p8)  //bottom four
+    {
+        addQuad(p1, p2, p3, p4);
+        addQuad(p8, p7, p6, p5);
+        addQuad(p2, p1, p5, p6);
+        addQuad(p3, p2, p6, p7);
+        addQuad(p4, p3, p7, p8);
+        addQuad(p1, p4, p8, p5);
+
+    }
+
+    public void addTriangle(Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        int oldSize = verts.Count;
+
+        verts.Add(p1);
+        verts.Add(p2);
+        verts.Add(p3);
+
+        tris.Add(oldSize);
+        tris.Add(oldSize + 1);
+        tris.Add(oldSize + 2);
+    }
+
+    public void addQuad(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
+    {
+        int oldSize = verts.Count;
+
+        verts.Add(p1);
+        verts.Add(p2);
+        verts.Add(p3);
+        verts.Add(p4);
+
+        tris.Add(oldSize);
+        tris.Add(oldSize + 1);
+        tris.Add(oldSize + 2);
+
+        tris.Add(oldSize);
+        tris.Add(oldSize + 2);
+        tris.Add(oldSize + 3);
+    }
+
+    public Mesh constructMesh()
+    {
+        Mesh newMesh = new Mesh();
+
+        newMesh.vertices = verts.ToArray();
+        newMesh.triangles = tris.ToArray();
+        newMesh.RecalculateNormals();
+        newMesh.RecalculateBounds();
+        newMesh.RecalculateTangents();
+
+        return newMesh;
+    }
+
+    //This takes data from the inputted mesh and copies it.
+    public void adopt(Mesh aMesh)
+    {
+        int offset = verts.Count;
+        //verts = new List<Vector3>();
+        //tris = new List<int>();
+
+        foreach(Vector3 v in aMesh.vertices)
+        {
+            verts.Add(v);
+        }
+
+        foreach (int i in aMesh.triangles)
+        {
+            tris.Add(i + offset);
+        }
+    }
+
+
+}
---
+++ theoreticalMeshTester.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class theoreticalMeshTester : MonoBehaviour {
+
+    public Mesh testMesh;
+
+    private MeshFilter myFilter;
+    private MeshRenderer myRenderer;
+
+	// Use this for initialization
+	void Start () {
+        theoreticalMesh t = new theoreticalMesh();
+        myFilter = gameObject.AddComponent<MeshFilter>();
+        myRenderer = gameObject.AddComponent<MeshRenderer>();
+        Material myMaterial = new Material(Shader.Find("Standard"));
+        myRenderer.material = myMaterial;
+
+        //t.adopt(testMesh);
+        t.addTriangle(Vector3.zero + Vector3.forward, Vector3.forward + Vector3.forward, Vector3.right + Vector3.forward);
+
+        t.addQuad(Vector3.forward + Vector3.up, Vector3.right + Vector3.up, Vector3.back + Vector3.up, Vector3.left + Vector3.up);
+
+        Vector3 offset1 = Vector3.down;
+        Vector3 offset2 = Vector3.down * 2;
+
+        t.addCube(Vector3.forward + offset1, Vector3.right + offset1, Vector3.back + offset1, Vector3.left + offset1,
+            Vector3.forward + offset2, Vector3.right + offset2, Vector3.back + offset2, Vector3.left + offset2);
+
+        List<Vector3> circle = new List<Vector3>();
+        for(int i = 0; i < 360; i += 10)
+        {
+            circle.Add(Quaternion.Euler(0, i, 0) * Vector3.forward);
+        }
+        t.addNgon(Vector3.zero, circle);
+
+        t.adopt(testMesh);
+
+        myFilter.mesh = t.constructMesh();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

d5b5b896839c296020d1c713ec169aee0e899509
q_andrew <andrewmanq@gmail.com>
2018-11-20 22:50:56 -0500

    Documentation update and implementation changes
    The test can now be run via context menu.

--- theoreticalMeshTester.cs
+++ theoreticalMeshTester.cs
@@ -2,41 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//This test class has many good examples of how to use the theoretical mesh
+
 public class theoreticalMeshTester : MonoBehaviour {
 
     public Mesh testMesh;
+    public Material testMaterial;
 
     private MeshFilter myFilter;
     private MeshRenderer myRenderer;
 
-	// Use this for initialization
 	void Start () {
-        theoreticalMesh t = new theoreticalMesh();
         myFilter = gameObject.AddComponent<MeshFilter>();
         myRenderer = gameObject.AddComponent<MeshRenderer>();
-        Material myMaterial = new Material(Shader.Find("Standard"));
-        myRenderer.material = myMaterial;
 
-        //t.adopt(testMesh);
+        performTest();
+	}
+
+    [ContextMenu("perform test")]
+    void performTest()
+    {
+        //this theoretical mesh will be manipulated and converted into a mesh at the end.
+        theoreticalMesh t = new theoreticalMesh();
+        myRenderer.material = testMaterial;
+
+        //-------------------------------ADD TRIANGLE
         t.addTriangle(Vector3.zero + Vector3.forward, Vector3.forward + Vector3.forward, Vector3.right + Vector3.forward);
 
+        //-------------------------------ADD QUAD
         t.addQuad(Vector3.forward + Vector3.up, Vector3.right + Vector3.up, Vector3.back + Vector3.up, Vector3.left + Vector3.up);
 
+        //-------------------------------ADD Cube
         Vector3 offset1 = Vector3.down;
         Vector3 offset2 = Vector3.down * 2;
 
         t.addCube(Vector3.forward + offset1, Vector3.right + offset1, Vector3.back + offset1, Vector3.left + offset1,
             Vector3.forward + offset2, Vector3.right + offset2, Vector3.back + offset2, Vector3.left + offset2);
 
+        //-------------------------------ADD NGON
         List<Vector3> circle = new List<Vector3>();
-        for(int i = 0; i < 360; i += 10)
+        for (int i = 0; i < 360; i += 10)
         {
             circle.Add(Quaternion.Euler(0, i, 0) * Vector3.forward);
         }
-        t.addNgon(Vector3.zero, circle);
+        t.addNgon(circle);
 
+        //-------------------------------ADD RIBBON
         List<Vector3> circleRibbon = new List<Vector3>();
-        for(int i = circle.Count - 1; i >= 0; i--)
+        circle.Add(Vector3.forward);
+        for (int i = circle.Count - 1; i >= 0; i--)
         {
             circleRibbon.Add(circle[i]);
             circleRibbon.Add(circle[i] + (Vector3.down * .5f));
@@ -55,13 +69,13 @@ public class theoreticalMeshTester : MonoBehaviour {
         rectangleRibbon.Add(topVec + Vector3.right * 3);
         rectangleRibbon.Add(bottomVec + Vector3.right * 3);
 
-        t.addRibbon(rectangleRibbon);
-
         rectangleRibbon.Add(topVec + Vector3.right * 4);
         rectangleRibbon.Add(bottomVec + Vector3.right * 4);
 
         rectangleRibbon.Add(topVec + Vector3.right * 4 + Vector3.up);
         rectangleRibbon.Add(bottomVec + Vector3.right * 4 + Vector3.up);
+
+        t.addRibbon(rectangleRibbon);
 
         for (int i = 0; i < rectangleRibbon.Count; i++)
         {
@@ -70,13 +84,10 @@ public class theoreticalMeshTester : MonoBehaviour {
 
         t.addRibbon(rectangleRibbon, 30);
 
+        //-------------------------------ADOPT MESH
         t.adopt(testMesh);
 
+        //-------------------------------CONSTRUCT MESH
         myFilter.mesh = t.constructMesh();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
